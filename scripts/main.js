0a235ab3b05ef7657c0fb17e5f9919d5d5a5d8fe
Ryan Thornton <74112014+ryansnoobcentral@users.noreply.github.com>
2023-04-29 18:59:42 -0400

    stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,11 @@
-// npm install blizzapi
-
 const searchBox = document.getElementById('top-search')
 
-// https://us.api.blizzard.com/data/wow/pvp-season/{pvpSeasonId}/pvp-leaderboard/{pvpBracket}?namespace=static-us
-// /data/wow/pvp-season/{pvpSeasonId}/pvp-leaderboard/{pvpBracket}
-
-
-// Gets authentication key from blizzard API
-const { BlizzAPI } = require("blizzapi");
-
-/**
- * Or using TypeScript:
- * import { BlizzAPI } from 'blizzapi';
- */
-
-const api = new BlizzAPI({
-  region: "us",
-  clientId: "2f7360b493cf41c9bc8941dc647dbb26",
-  clientSecret: "rEEM3k7nfo1WJaTpSQR1PR53vlwZ8KRA",
-});
-
-const data = await api.query("/path/to/endpoint");
-
-console.log(data);
-
-fetch("https://us.api.blizzard.com/data/wow/pvp-season/33?namespace=dynamic-us&locale=en_US")
-    .then(parseJson)
-    .then()
-    .catch(() => {
-      console.log("something is fucked");
-    });
+// fetch("https://us.api.blizzard.com/data/wow/pvp-season/33?namespace=dynamic-us&locale=en_US")
+//     .then(parseJson)
+//     .then()
+//     .catch(() => {
+//       console.log("something is fucked");
+//     });
 
 // Parses JSON.
 function parseJson(promise) {
