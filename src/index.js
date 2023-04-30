658a2fd014937dd45def037285714c23668f38f5
Ryan Thornton <74112014+ryansnoobcentral@users.noreply.github.com>
2023-04-29 21:54:48 -0400

    Got a bunch of items working.... Next is the API calls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 // RUN npx webpack EVERYTIME THIS IS FILE IS CHANGED.  WEBPACK ALLOWS NODE.JS TO BE RAN WITHIN THE BROWSER.
+const searchBox = document.getElementById('top-search')
+let pair;
 
+searchBox.onsubmit = (ev) => {
+    console.log('submitted top-search with', ev)
+    // https://stackoverflow.com/a/26892365/1449799
+    const formData = new FormData(ev.target)
+    console.log(formData)
+    // This logs the FormData object's values into the variable of pair.
+    for (pair of formData.entries()) {
+        console.log(`${pair[0]}, ${pair[1]}`);
+    }
+    ev.preventDefault()
+}
 
+// This gathers a new key to use the Blizzard API.
 const {BlizzAPI} = require("blizzapi");
 
 const BnetApi = new BlizzAPI({
@@ -9,37 +23,12 @@ const BnetApi = new BlizzAPI({
     clientSecret: "rEEM3k7nfo1WJaTpSQR1PR53vlwZ8KRA",
 });
 
-// Used to query or more or less known as "fetch" the URL and then use the promise to extract the JSON data
+// Used to query or more or less known as "fetch" the URL and then use the promise to extract the JSON data.  The
+// API has a default time out of 10 seconds if this is not found.
 BnetApi.query("/sc2/profile/1/2/242838")
-    .then(parseJson)
-    .then(handleJson);
+    .then(parseData)
 
-const searchBox = document.getElementById('top-search')
-
-// fetch("https://us.api.blizzard.com/data/wow/pvp-season/33?namespace=dynamic-us&locale=en_US")
-//     .then(parseJson)
-//     .then()
-//     .catch(() => {
-//       console.log("something is fucked");
-//     });
-
-// Parses JSON.
-function parseJson(promise) {
-    console.log(promise);
-    return promise.json();
-}
-
-function handleJson(data) {
+// Parses data from API.
+function parseData(data) {
     console.log(data);
-}
-
-searchBox.onsubmit = (ev) => {
-    console.log('submitted top-search with', ev)
-    // https://stackoverflow.com/a/26892365/1449799
-    const formData = new FormData(ev.target)
-    console.log(formData)
-    for (const pair of formData.entries()) {
-        console.log(`${pair[0]}, ${pair[1]}`);
-    }
-    ev.preventDefault()
 }
