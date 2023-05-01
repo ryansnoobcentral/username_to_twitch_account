87e78995287c18b11312d7be9a9a682dccc8ec0c
Ryan Thornton <74112014+ryansnoobcentral@users.noreply.github.com>
2023-04-30 22:36:31 -0400

    Character / realm lists / others updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // RUN npx webpack EVERYTIME THIS IS FILE IS CHANGED.  WEBPACK ALLOWS NODE.JS TO BE RAN WITHIN THE BROWSER.
 
+// Const for twitch div.
+const twitchDiv = document.getElementById('twitch-embed');
+// Const for search within dropdown
+const dropdownSearch = document.getElementById('dropdownSearch');
 // Const for realms.
 const realms = document.getElementById('realm');
 // Const for realm-title.
@@ -19,19 +23,29 @@ let regionType = "us";
 // Variable for search bar elements inputted data.
 let curChar = ["", "utterlycrazy"];
 // Variable for leaderboards select.
-let leaderboardType = "shuffle";
+let leaderboardType = "3v3";
 // Variable of all WOW realms.
 let allRealms;
 // Variable of current realm selected.
 let curRealm = "illidan";
 // Variable for search string.
-// /profile/wow/character/{realmSlug}/{characterName}/pvp-bracket/{pvpBracket}
-let charQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "/pvp-bracket/"
+// /profile/wow/character/{realmSlug}/{characterName}
+let charQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "?namespace=profile-" + regionType;
+// Variable for pvp string.
+let pvpQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "/pvp-bracket/"
     + leaderboardType + "?namespace=profile-" + regionType;
-// Client ID.
+// Variable for character render.
+// /profile/wow/character/{realm-slug}/{character-name}/character-media
+let charRenderImage = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "/character-media"
+    + "?namespace=profile-" + regionType;
+// Variable Client ID.
 let clientID = "2f7360b493cf41c9bc8941dc647dbb26";
-// Client Secret.
+// Variable Client Secret.
 let clientSecret = "rEEM3k7nfo1WJaTpSQR1PR53vlwZ8KRA";
+// Variable for dropdown items.
+let dropdownItems;
+// Variable for dropdown dividers.
+let dropdownDivider;
 
 
 // Search box action listener.
@@ -46,13 +60,25 @@ searchBox.onsubmit = (ev) => {
     }
     ev.preventDefault();
     updateCharQueryString();
+    updatePvpQueryString();
     // I used the supplied libraries method of query, which ends up returning a JSON instead of a promise like fetch.
     // However, in a sense this is just fetch with the return promise.json(). The API has a default time out of 10 seconds
     // if this is not found.
     BnetApi.query(charQueryString)
         .then(parseCharacter)
         .catch((error) => {
-            console.log("Query Failed", error);
+            console.log("Character Query Failed", error);
+
+        });
+    // Query's Image of character.
+    BnetApi.query(charRenderImage)
+        .then((ev) => {
+            console.log("Character Image parsed", ev.assets[0]);
+            twitchDiv.style.background = ev.assets[0].value;
+        })
+        .catch((error) => {
+            console.log("Character Image Query Failed", error);
+
         });
 }
 
@@ -61,27 +87,67 @@ leaderboards.onclick = (ev) => {
     console.log("Leaderboards ", ev.target.innerText);
     leaderboardType = ev.target.innerText.toLowerCase();
     leaderboardsTitle.innerText = "Leaderboards: " + leaderboardType;
-    updateCharQueryString();
+    updatePvpQueryString();
 }
 
 // Region dropdown action listener.
 region.onclick = (ev) => {
     console.log("Region selected", ev.target.innerText);
-    regionType = ev.target.innerText;
-    regionTitle.innerText = "Region: " + regionType;
+    regionType = ev.target.innerText.toLowerCase();
+    regionTitle.innerText = "Region: " + regionType.toUpperCase();
     BnetApi = new BlizzAPI({
-        region: regionType.toLowerCase(),
+        region: regionType,
         clientId: clientID,
         clientSecret: clientSecret,
     });
+    // Updates realm list.
+    BnetApi.query("/data/wow/realm/index?namespace=dynamic-" + regionType)
+        .then(parseRealms)
+        .catch(error => {
+            console.log("Error getting realms", error);
+        });
     updateCharQueryString();
+    updatePvpQueryString();
 }
 
 // Realms dropdown action listener.
 realms.onclick = (ev) => {
-    console.log("Realm selected", ev.target.innerText);
-    //TODO handle realms
+    curRealm = ev.target.innerText.toLowerCase()
+        .replace(" ", "-")
+        .replace("'", "");
+    console.log("Realm selected", curRealm);
+    realmsTitle.innerText = "Realm: " + ev.target.innerText;
+    updateCharQueryString();
+    updatePvpQueryString();
 }
+
+// Search within realms dropdown listener.
+dropdownSearch.addEventListener("input", function () {
+    const searchTerm = dropdownSearch.value.toLowerCase();
+
+    let i = 0;
+    dropdownItems.forEach(function (item) {
+        i++;
+        const text = item.textContent.toLowerCase();
+
+        if (text.indexOf(searchTerm) !== -1) {
+            item.style.display = "block";
+            // Used so it stops printing an annoying error when I don't care.
+            try {
+                dropdownDivider[i].style.display = "block";
+            } catch (error) {
+            }
+        } else {
+            item.style.display = "none";
+            // Used so it stops printing an annoying error when I don't care.
+            try {
+                dropdownDivider[i].style.display = "none";
+            } catch (error) {
+            }
+        }
+    });
+});
+
 // This gathers a new key to use the Blizzard API.
 const {BlizzAPI} = require("blizzapi");
 
@@ -103,14 +169,36 @@ function parseCharacter(data) {
 }
 
 // Parses realm data from API.
-function parseRealms(realms) {
-    console.log(realms);
-    allRealms = realms.realms.map(realm => realm.name.en_US);
+function parseRealms(realmsList) {
+    console.log(realmsList);
+    allRealms = realmsList.realms.map(realm => realm.name.en_US);
     console.log("Current realm list for " + regionType, allRealms);
+    // Populates realm list in dropdown.
+    for (let i = 0; i < allRealms.length; i++) {
+        let listItem = document.createElement("li");
+        let button = document.createElement("button");
+        let hr = document.createElement("hr");
+        hr.className = "dropdown-divider";
+        button.className = "dropdown-item";
+        button.innerText = allRealms[i];
+        listItem.append(button);
+        listItem.append(hr);
+        realms.append(listItem);
+    }
+    dropdownItems = document.querySelectorAll('#realm .dropdown-item');
+    dropdownDivider = document.querySelectorAll('#realm .dropdown-divider');
 }
 
 // Updates character query string.
 function updateCharQueryString() {
-    charQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "/pvp-bracket/"
+    charQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1].toLowerCase()
+        + "?namespace=profile-" + regionType;
+    charRenderImage = charRenderImage = "/profile/wow/character/" + curRealm + "/" + curChar[1] + "/character-media"
+        + "?namespace=profile-" + regionType;
+}
+
+// Updates pvp query string.
+function updatePvpQueryString() {
+    pvpQueryString = "/profile/wow/character/" + curRealm + "/" + curChar[1].toLowerCase() + "/pvp-bracket/"
         + leaderboardType + "?namespace=profile-" + regionType;
 }
